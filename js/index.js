d8fb1bdb707cf28fa07ca3fb0fd6c70d10354619
rkshockey <rkshockey@gmail.com>
2021-07-06 17:04:44 -0400

    Got 5 events working

--- js/index.js
+++ js/index.js
@@ -1,25 +1,45 @@
-// Playing with the buttons
-allButtons = Array.from(document.querySelectorAll(`.btn`))
-for (let i = 0; i < allButtons.length; i++){
-    allButtons[i] = document.querySelector(`.btn:nth-of-type(${i + 1})`)
-}
-funButton = document.querySelector(`.fun`)
+// Specifying buttons
+funButton = document.querySelector(`.fun`);
 mountainButton = document.querySelector(`.mountain`)
-islandButton = document.querySelector(`.island`)
-destinations = Array.from(document.querySelectorAll(`.destination`))
-for (let i = 0; i < destinations.length; i++){
-    destinations[i] = document.querySelector(`.destination:nth-of-type(${i + 1})`)
+islandButton = document.querySelector(`.island`);
+divs = document.querySelectorAll(`.destination`)
+for (let i = 0; i < divs.length; i++){
+    divs[i] = document.querySelector(`.destination:nth-of-type(${i + 1})`)
 }
+// Mouse Enter and Leave
+function colorTo (event){
+    divs[0].style.background = `blue`;
+    divs[1].style.background = `green`;
+    divs[2].style.background = `cyan`;
+}
+function colorBack (event){
+    divs[0].style.background = `white`;
+    divs[1].style.background = `white`;
+    divs[2].style.background = `white`;
+}
+funButton.addEventListener(`mouseenter`, colorTo);
+mountainButton.addEventListener(`mouseenter`, colorTo);
+islandButton.addEventListener(`mouseenter`, colorTo);
+funButton.addEventListener(`mouseleave`, colorBack);
+mountainButton.addEventListener(`mouseleave`, colorBack);
+islandButton.addEventListener(`mouseleave`, colorBack);
 
-for (let i = 0; i < allButtons.length; i++){
-    allButtons[i].addEventListener(`mouseover`, (event) => {
-        destinations[0].style.background = `blue`;
-        destinations[1].style.background = `green`;
-        destinations[2].style.background = `cyan`;
-})}
-for (let i = 0; i < allButtons.length; i++){
-    allButtons[i].addEventListener(`mouseleave`, (event) => {
-        destinations[0].style.background = `white`;
-        destinations[1].style.background = `white`;
-        destinations[2].style.background = `white`;
-})}
+// Mouse click, double-click, and up
+function green (event) {
+    divs[0].querySelector(`h4`).style.color = `green`;
+    divs[0].querySelector(`p`).style.color = `green`;
+}
+funButton.addEventListener(`click`, green);
+
+function cyan (event) {
+    divs[1].querySelector(`h4`).style.color = `cyan`;
+    divs[1].querySelector(`p`).style.color = `cyan`;
+}
+mountainButton.addEventListener(`dblclick`, cyan);
+
+function blue (event) {
+    divs[2].querySelector(`h4`).style.color = `blue`;
+    divs[2].querySelector(`p`).style.color = `blue`;
+}
+islandButton.addEventListener(`mouseup`, blue)
+
